5b775dbd10dc613443af5630c9f1d62548ef3e41
Nym29 <D17124792@mydit.ie>
2022-02-05 17:12:09 +0000

    initial commit

---
+++ SuggestionAppLibrary/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using SuggestionAppLibrary.Models;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+global using MongoDB.Driver;
+global using System;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
---
+++ SuggestionAppUI/Program.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+
+//namespace SuggestionAppUI
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CreateHostBuilder(args).Build().Run();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    webBuilder.UseStartup<Startup>();
+//                });
+//    }
+//}
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SuggestionAppUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.ConfigureServices();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+
+app.MapControllers();
+
+app.Run();
