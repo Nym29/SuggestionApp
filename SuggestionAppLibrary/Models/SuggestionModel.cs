8d3804ee57a6a50e0b1a2fc49ea0f98fd37584f5
Nym29 <D17124792@mydit.ie>
2022-02-13 19:41:04 +0000

    fixed filtering and status issues

--- SuggestionAppLibrary/Models/SuggestionModel.cs
+++ SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -14,10 +14,10 @@ public class SuggestionModel
     public CategoryModel Category { get; set; }
     public BasicUserModel Author { get; set; }
     public HashSet<string> UserVotes { get; set; } = new();
-    public StatusModel? SuggestionStatus { get; set; }
     public string OwnerNotes { get; set; }
     public bool ApprovedForRelease { get; set; } = false;
     public bool Rejected { get; set; } = false;
     public bool Archived { get; set; } = false;
     public StatusModel Status { get; set; }
+    public StatusModel SuggestionStatus { get; set; }
 }
